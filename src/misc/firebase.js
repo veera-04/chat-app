57cc1fddd76846c3c11dfea0e194c9506b0d075f
Veera <veera4721@gmail.com>
2021-05-22 19:23:06 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyA1Yg1foivhHKaTa4EgC2Gbk_xz4CEja4I',
+  authDomain: 'chat-app-1570a.firebaseapp.com',
+  projectId: 'chat-app-1570a',
+  storageBucket: 'chat-app-1570a.appspot.com',
+  messagingSenderId: '831536017055',
+  appId: '1:831536017055:web:27ec4e54aab8bd6dbb7caa',
+};
+
+firebase.initializeApp(config);
