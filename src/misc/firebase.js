89b8da10d0eb1fb257f8f3174f346521dcf620f9
Veera <veera4721@gmail.com>
2021-05-24 12:38:30 +0530

    Added sign in page interactions

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyA1Yg1foivhHKaTa4EgC2Gbk_xz4CEja4I',
@@ -9,4 +11,6 @@ const config = {
   appId: '1:831536017055:web:27ec4e54aab8bd6dbb7caa',
 };
 
-firebase.initializeApp(config);
+const app = firebase.initializeApp(config);
+export const auth = app.auth();
+export const database = app.database();
