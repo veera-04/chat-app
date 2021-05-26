c9f1d9713ae4e263e18a64d0b0e3d9702349dffe
Veera <veera4721@gmail.com>
2021-05-26 20:33:30 +0530

    added bottom text-bar and displayed last msg in dashboard

---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Messages</div>;
+};
+
+export default index;
---
+++ src/pages/Home/Chat.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Loader } from 'rsuite';
+import { useParams } from 'react-router';
+import { useRooms } from '../../context/rooms.context';
+import ChatTop from '../../components/chat-window/top';
+import ChatBottom from '../../components/chat-window/bottom';
+import Messages from '../../components/chat-window/messages';
+import { CurrentRoomProvider } from '../../context/current-room.context';
+
+const Chat = () => {
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} Not found </h6>;
+  }
+
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
+
+  return (
+    <CurrentRoomProvider data={currentRoomData}>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </CurrentRoomProvider>
+  );
+};
+
+export default Chat;
