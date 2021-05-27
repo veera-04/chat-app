62162a0e0341c4db99cb3cdda3f931ee77120ec3
Veera <veera4721@gmail.com>
2021-05-27 17:30:35 +0530

    added button to grant/revoke admin permission

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -6,6 +6,8 @@ import ChatTop from '../../components/chat-window/top';
 import ChatBottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
 import { CurrentRoomProvider } from '../../context/current-room.context';
+import { transformToArr } from '../../misc/helpers';
+import { auth } from '../../misc/firebase';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -24,9 +26,14 @@ const Chat = () => {
 
   const { name, description } = currentRoom;
 
+  const admins = transformToArr(currentRoom.admins);
+  const isAdmin = admins.includes(auth.currentUser.uid);
+
   const currentRoomData = {
     name,
     description,
+    admins,
+    isAdmin,
   };
 
   return (
